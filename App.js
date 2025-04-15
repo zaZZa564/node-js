9793599fba2a77dbc7f6e31e1f9c1ad612284820
zaZZa564 <anus_bobra564@mail.ru>
2025-04-15 22:24:18 +0300

    global objects

--- App.js
+++ App.js
@@ -1,2 +1,6 @@
 // node App.js --> log
 console.log('log');
+
+// глобальные объекты
+console.log(`Директория: ${__dirname}`); // Directory: C:\Users\barab\OneDrive\Документы\repos\node-js
+console.log(`Аргументы CLI: ${process.argv}`); // Массив аргументов командной строки
