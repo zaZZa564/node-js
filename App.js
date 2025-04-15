34c09bc1aa64e10bedd74298337e06c806504be6
zaZZa564 <anus_bobra564@mail.ru>
2025-04-15 22:29:11 +0300

    ES-6 modules

--- App.js
+++ App.js
@@ -4,3 +4,7 @@ console.log('log');
 // глобальные объекты
 console.log(`Директория: ${__dirname}`); // Directory: C:\Users\barab\OneDrive\Документы\repos\node-js
 console.log(`Аргументы CLI: ${process.argv}`); // Массив аргументов командной строки
+
+// ES-6 модули
+import { sum } from "./sum";
+console.log(sum(5, 10)); // 15
